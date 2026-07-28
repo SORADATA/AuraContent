20cd78023e255099f925fa7d72e0543a50302e39
sora <moussa.sissoko6@etu.univ-lorraine.fr>
2026-07-28 11:53:50 +0000

    fix: exception & raise function

--- publish.py
+++ publish.py
@@ -1,26 +1,47 @@
 import os
 import requests
+from datetime import datetime
 from constants import API_URL, DIRECT_URL
 
 
 def get_latest_video_url():
     """
-    Interroge l'api hugging face pour trouver la dernière vidéo
+    Interroge l'api hugging face pour trouver la vidéo à publier selon l'heure
     """
     api_url = API_URL
     response = requests.get(api_url)
-    if response.status_code in [200, 201]:
-        print("✅ Succès ! La vidéo a été publiée sur le compte TikTok @minute_mystereko.")
-    else:
-        raise Exception(f"❌ Errors during publishing {response.status_code} : {response.text}")
+
+    if response.status_code != 200:
+        raise Exception(f" Do not read folder on HF : {response.status_code}")
+
     files = response.json()
+
     # Filter to take only .mp4 file
     videos = [f['path'] for f in files if f['path'].endswith('.mp4')]
-    videos.sort()
-    # Take recent video
-    latest_video_path = videos[-1]
-    direct_url = f"{DIRECT_URL}{latest_video_path}" 
-    return direct_url, latest_video_path
+
+    # Curreent day format YYYYMMDD
+    today_str = datetime.utcnow().strftime("%Y%m%d")
+
+    # Take only videos today generated
+    todays_videos = [v for v in videos if today_str in v]
+    todays_videos.sort()
+
+    if not todays_videos:
+        raise Exception(" No video found on HF.")
+
+    # Current hour (en UTC)
+    current_hour = datetime.utcnow().hour
+
+    # Selection video: 1ère vidéo à midi, 2ème vidéo le soir
+    if len(todays_videos) >= 2 and current_hour >= 15:
+        target_video_path = todays_videos[1]
+        print("🌙 Evening Exécution  : publication of 2d  daily video.")
+    else:
+        target_video_path = todays_videos[0]
+        print("☀️ Midle Exécution: publication of 1st daily video.")
+
+    direct_url = f"{DIRECT_URL}{target_video_path}"
+    return direct_url, target_video_path
 
 
 def publish_to_tiktok():
@@ -29,17 +50,16 @@ def publish_to_tiktok():
     print(f"🎥 Daily video found : {video_url}")
 
     api_key = os.environ.get("ZERNIO_API_KEY")
-    # ⚠️ CORRECTION 3 : Correction de la faute de frappe (ACCOUNT avec un T)
     account_id = os.environ.get("TIKTOK_ACCOUNT_ID") 
 
     if not api_key or not account_id:
-        raise ValueError("Zernio or tiktok api keys not found")
+        raise ValueError(" Zernio or tiktok api keys not found")
 
     # Cleaning filename output
     raw_filename = file_path.split("/")[-1]
     clean_title = raw_filename.replace(".mp4", "").replace("_", " ")[16:]
     caption = f"{clean_title} 🧠✨ #IA #MinuteMystère #Decouverte"
-    print(f"📝 Légende générée : {caption}")
+    print(f"📝 Légende generated : {caption}")
 
     # Sending to zernio api for publish on tiktok
     url = "https://zernio.com/api/v1/posts"
@@ -66,8 +86,11 @@ def publish_to_tiktok():
 
     print("🚀 Envoi de la requête à Zernio...")
     response = requests.post(url, headers=headers, json=payload)
-    if response.status_code == 200:
+
+    if response.status_code in [200, 201]:
         print("✅ Succès ! La vidéo a été publiée sur le compte TikTok @minute_mystereko.")
+    elif response.status_code == 409:
+        print("⚠️ Zernio a bloqué la publication : Cette vidéo a déjà été publiée récemment (Doublon).")
     else:
         raise Exception(f"❌ Errors during publishing {response.status_code} : {response.text}")
 
