e4966a26a14f69a188362fbd4d23655ee93dad1b
MoussaTheAnalyst <163172464+SORADATA@users.noreply.github.com>
2026-08-04 21:04:20 +0200

    Add timeout to HTTP GET request

--- modules/utils/zernio_client_finance.py
+++ modules/utils/zernio_client_finance.py
@@ -20,7 +20,7 @@ def get_latest_videos_stats():
     }
 
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, timeout=15)
         response.raise_for_status()
         data = response.json()
 
